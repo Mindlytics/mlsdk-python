36e3ff72fe87cdfb2737600fe15b5e3924619363
Andrew Peebles <aqpeeb@gmail.com>
2025-05-07 15:47:57 -0700

    fix tests so they will run in CI

--- tests/test_with_test_server.py
+++ tests/test_with_test_server.py
@@ -10,10 +10,17 @@ logger = logging.getLogger(__name__)  # Use module name
 server = os.getenv("SERVER_BASE")
 api_key = None
 
+if server is None:
+    logger.warning(
+        "SERVER_BASE environment variable is not set. Skipping tests that require a server."
+    )
+
 
 @pytest.mark.asyncio
 async def test_basic_auth_failure():
     # Test session as context manager
+    if server is None:
+        pytest.skip("SERVER_BASE environment variable is not set.")
     client = Client(api_key="test_api_key", debug=True, server_endpoint=server)
     session_context = client.create_session(
         session_id="test_session", project_id="test_project"
@@ -32,6 +39,8 @@ async def test_basic_auth_failure():
 
 @pytest.mark.asyncio
 async def test_get_api_key():
+    if server is None:
+        pytest.skip("SERVER_BASE environment variable is not set.")
     global api_key
     api_key = await get_api_key("org-1")
     assert api_key is not None
@@ -42,6 +51,8 @@ async def test_get_api_key():
 @pytest.mark.asyncio
 async def test_basic_event_failure():
     # Test session as context manager
+    if server is None:
+        pytest.skip("SERVER_BASE environment variable is not set.")
     client = Client(api_key=api_key, debug=True, server_endpoint=server)
     session_context = client.create_session(
         session_id="test_session", project_id="test_project"
